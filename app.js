bbedfdbf8a4019a8ebd600fafe60c3eb1d480c6b
unknown <20knojinomono01@gmail.com>
2026-01-29 02:31:35 +0900

    add env

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catRouter = require('./routes/cat');
 var dogRouter = require('./routes/cat');
+var notesRouter = require('./routes/notes');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cat',catRouter);
 app.use('/dog',dogRouter);
+app.use('/notes', notesRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
